4067d4f8f3476f7a232e65c595f01975252c09ed
zhornyk-maria <zhornyk.maria@gmail.com>
2023-12-27 13:08:02 +0200

    tasks 2 and 3

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,28 @@
 
+class Storage {
+  #items;
+  constructor(initialItems) {
+    this.#items = initialItems;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const indexToRemove = this.#items.indexOf(itemToRemove);
+    if (indexToRemove !== -1) {
+      this.#items.splice(indexToRemove, 1);
+    }
+  }
+}
+
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
